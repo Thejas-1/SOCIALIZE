e613be606c83006d86684d2084549b10b4f01ae4
Thejas-1 <naiktheju@gmail.com>
2017-02-15 02:06:35 +0530

    SOCIALIZE, first commit

---
+++ blog/models.py
@@ -0,0 +1,6 @@
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
