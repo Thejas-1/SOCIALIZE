d11c7bdb8f3a4e88d9881058c2b3a81ad7d62ed1
Thejas-1 <naiktheju@gmail.com>
2017-03-08 01:01:31 +0530

    Added a html file

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,16 @@
-# Create your models here.
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-    
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
